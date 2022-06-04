79712af5a0aa8cba696d966191e060e545d38a40
Ezequiel <ezequiel.neuhauser@gmail.com>
2022-06-04 18:48:23 -0300

    primer commit subiendo los primeros archivos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo3 from './Assets/img/logo3.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nautica Leo</h1>
+        <div className="logo-header"><img src={logo3}></img></div>
+        <p>Guardería Náutica y Taller Mecanico</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
